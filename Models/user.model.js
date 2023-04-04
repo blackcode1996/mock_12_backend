45874cc9fc69c9cba0938184b16a1ca90d8d5e0b
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-04-04 18:38:27 +0530

    done

--- Models/user.model.js
+++ Models/user.model.js
@@ -3,7 +3,11 @@ const mongoose=require("mongoose")
 const UserSchema=mongoose.Schema({
     name:{type:String},
     email:{type:String},
-    password:{type:String}
+    password:{type:String},
+    loggedInAt: {
+        type: Date,
+        default: Date.now,
+    }
 })
 
 const UserModel=mongoose.model("user",UserSchema)
