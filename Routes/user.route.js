c8fffb43fef51e1d750599dc335a7d49b1896685
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-04-06 13:11:50 +0530

    did

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -6,12 +6,10 @@ const jwt=require("jsonwebtoken")
 
 const bcrypt=require("bcrypt")
 
-const { authenticate } = require("../Middlewares/authenticate.middleware")
-
 const userRouter=express.Router()
 
 userRouter.post("/register",async(req,res)=>{
-    const {name,email,password}=req.body
+    const {email,password}=req.body
 
     const existUser=await UserModel.findOne({email:email})
 
@@ -50,48 +48,6 @@ userRouter.post("/login",async(req,res)=>{
         res.send(400).status({"msg":error.message})
     }
 })
-
-userRouter.get("/getProfile",authenticate,async(req,res)=>{
-
-    const user=await UserModel.findOne({_id:req.body.user})
-    
-    try {
-        if(user){
-            res.json({user,success:true})
-        }else{
-            res.json({"msg":"Invalid user",success:false})
-        }
-    } catch (error) {
-        res.json({"msg":"error occured",success:false})
-    }
-    
-})
-
-userRouter.post('/calculate',authenticate,async(req,res)=>{
-
-    const { amount, rate, years } = req.body;
-
-    const i = rate / 100;
-    const n = years;
-    const P = amount;
-  
-    const F = P * ((Math.pow(1 + i, n) - 1) / i);
-    const totalInvestment = P * n;
-    const totalInterest = F - totalInvestment;
-
-    try {
-        res.json({
-            totalInvestment: totalInvestment.toFixed(0),
-            totalInterest: totalInterest.toFixed(0),
-            maturityValue: F.toFixed(0),
-          });
-    } catch (error) {
-        res.json({"msg":error.message})
-    }
-  
-   
-  });
-   
 
 
 module.exports={
